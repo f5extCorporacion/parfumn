d256b001e51c22f74204305072c280d6c68a99e2
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2024-02-13 15:32:06 -0600

    Update Home.jsx
    apli para que me llamen

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,15 +12,7 @@ const Home = ({tema , setTheme}) => {
   return(
     <>     
     <div className="mascara flex flex-wrap justify-center items-center p-2  rounded-2xl  ">
-      <div className="derecho">
-         <figure className='w-60 p-10  '>
-          <img  src={siluetafrank} alt=""  />
-         </figure>
-      </div>
-      <div className="izquierdo flex flex-col bg-green-500 opacity-70 text-white border border-white p-2 rounded-lg">
-        <bdo className='text-blue-700 text-4xl  p-2  sm:text-2xl'>Frank Valverde</bdo>
-        <dfn className=' text-white '>Fullstack Developer</dfn>
-      </div>
+     
     </div>
     </>
   )
