1409d3b49dfd2945d46e81f89a4b0bb4a2816e9e
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2024-06-15 14:06:02 -0500

    Create ProyectosBase.jsx
    creacion de proyectos

---
+++ src/ProyectosBase.jsx
@@ -0,0 +1,35 @@
+export const Proyectos = [
+    {
+      id: 1,
+      name: "Usuarios add",
+      url: "https://crud-api-rho.vercel.app/",
+      img: "https://www.iconpacks.net/icons/1/free-user-group-icon-296-thumb.png",
+      detalle: "crud creado con react, tailwindcss, html, css.",
+      categoria: "fullstack",
+    },
+    {
+      id: 2,
+      name: "Frases ramdon",
+      url: "https://gen-react30.vercel.app/",
+      img: "https://parspng.com/wp-content/uploads/2022/11/bookpng.parspng.com-9.png",
+      detalle: "crud creado con react, tailwindcss, html, css.",
+      categoria: "fullstack",
+    },
+    {
+      id: 3,
+      name: "Front Noche",
+      url: "https://www.bombomsexxx.com/fotosPorfolio/muestras/moon/",
+      img: "https://purepng.com/public/uploads/large/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-1411527066895cj9zn.png",
+      detalle: "crud creado con react, tailwindcss, html, css.",
+      categoria: "front",
+    },
+    {
+      id: 4,
+      name: "Alterno",
+      url: "https://www.bombomsexxx.com/fotosPorfolio/muestras/insigni/",
+      img: "https://images.vexels.com/media/users/3/142887/isolated/preview/fc58c5ffb8c2e33fc3e15a2453189825-logotipo-de-empresa-logistica-en-crecimiento.png",
+      detalle: "Card modulable responsive creado con html y css codigo limpio",
+      categoria: "front",
+    },
+  ];
+  
